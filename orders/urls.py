fd3a8920498053bc92ab8bb7fe50e35fb7dcffb1
alexandrepernin <alex@lazylantern.com>
2020-05-09 00:33:22 +0200

    Add login and logout mechanism

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 #urlpatterns: list of the routes. views contains the method to route for the associated route
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,29 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 from .models import Pizza
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return render(request, "orders/login.html", {"message": None})
     context = {
+        "user": request.user,
         "Pizzas": Pizza.objects.all()
     }
     return render(request, "orders/index.html", context)
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "orders/login.html", {"message": "Invalid credentials."})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "orders/login.html", {"message": "Logged out."})
