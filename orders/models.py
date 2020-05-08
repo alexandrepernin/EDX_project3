6f33de012b467b00eea4caedd3330f5e13d31ed1
alexandrepernin <alex@lazylantern.com>
2020-05-08 13:27:49 +0200

    First Pizza model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Pizza(models.Model):
+    type = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    toppings = models.IntegerField()
+    size = models.CharField(max_length=64)
+
+    def __str__(self):
+        return "Pizza {}, {} with {} toppings of size {}".format(self.type, self.name, self.toppings, self.size)
--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+#urlpatterns: list of the routes. views contains the method to route for the associated route
 urlpatterns = [
     path("", views.index, name="index")
 ]
