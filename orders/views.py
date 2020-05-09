f9b15c9976fdf33062867cde6a880da43777d0b4
alexandrepernin <alex@lazylantern.com>
2020-05-09 14:54:04 +0200

    Add base for sign up mechanism

--- orders/views.py
+++ orders/views.py
@@ -15,14 +15,26 @@ def index(request):
     return render(request, "orders/index.html", context)
 
 def login_view(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+    if request.method=="GET":
+        return render(request, "orders/login.html")
     else:
-        return render(request, "orders/login.html", {"message": "Invalid credentials."})
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "orders/login.html", {"message": "Invalid credentials."})
+
+def signup_view(request):
+    if request.method=="GET":
+        return render(request, "orders/signup.html")
+    else:
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        return HttpResponseRedirect(reverse("index"))
 
 def logout_view(request):
     logout(request)
