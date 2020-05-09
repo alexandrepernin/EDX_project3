7b00ed94c7f00c9abfed90f68a4603051d2194aa
alexandrepernin <alex@lazylantern.com>
2020-05-09 18:44:11 +0200

    Create models for menu

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Pizza
+from .models import Size, Pizza, Topping, Pasta, Salad, Dinner
 # Register your models here.
-
+admin.site.register(Size)
 admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(Pasta)
+admin.site.register(Salad)
+admin.site.register(Dinner)
--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Size(models.Model):
+    name=models.CharField(max_length=64)
+
+    def __str__(self):
+        return "Size: {}".format(self.name)
 
 class Pizza(models.Model):
     type = models.CharField(max_length=64)
     name = models.CharField(max_length=64)
     toppings = models.IntegerField()
-    size = models.CharField(max_length=64)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
 
     def __str__(self):
         return "Pizza {}, {} with {} toppings of size {}".format(self.type, self.name, self.toppings, self.size)
+
+class Topping(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return "Topping #{}: {}".format(self.id, self.name)
+
+class Pasta(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField()
+
+    def __str__(self):
+        return "Pasta #{}: {}".format(self.id, self.name)
+
+class Salad(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField()
+
+    def __str__(self):
+        return "Salad #{}: {}".format(self.id, self.name)
+
+class Dinner(models.Model):
+    name = models.CharField(max_length=64)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    price = models.FloatField()
+
+    def __str__(self):
+        return "Dinner Platter #{}: {}. {}.".format(self.id, self.name, self.size)
